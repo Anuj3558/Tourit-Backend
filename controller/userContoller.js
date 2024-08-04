585ef07af9e8b078397f9079f69ebc3e5b6bb783
Anuj3558 <127491755+Anuj3558@users.noreply.github.com>
2024-08-04 23:51:46 +0530

    Update userContoller.js

--- controller/userContoller.js
+++ controller/userContoller.js
@@ -82,7 +82,7 @@ const handleLogin = async (req, res) => {
 
 const handleAuth = async (req, res) => {
     try {
-        const token = req.cookies;
+        const token = req.params;
         
         // if (!token) {
         //     return res.status(400).send("No token");
