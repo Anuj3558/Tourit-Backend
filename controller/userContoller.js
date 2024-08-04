b552f58e5e01c4dafc8786249ea12f9d6bd6be6a
Anuj3558 <127491755+Anuj3558@users.noreply.github.com>
2024-08-04 22:17:28 +0530

    Update userContoller.js

--- controller/userContoller.js
+++ controller/userContoller.js
@@ -71,7 +71,7 @@ const handleLogin = async (req, res) => {
             userEmail,
             pass
         }, process.env.SECERET_KEY, { expiresIn: '24h' })
-        res.status(200).send(id);
+        res.status(200).send(id).cookie('_id', id, { httpOnly: true });
     }
     catch (err) {
         res.status(500).send("Internal server error");
